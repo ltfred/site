113c9b75bf61b58924b7b75c806089b98eeffaa2
ltfred <ltfred@163.com>
2020-10-15 22:55:50 +0800

    feat: 添加获取下一个节日的信息

--- apps/apiv2/urls.py
+++ apps/apiv2/urls.py
@@ -3,6 +3,7 @@ from django.urls import re_path, path
 from apiv2.views import HolidayView, DateView
 
 urlpatterns = [
-    re_path(r"^holiday/year/$", HolidayView.as_view()),
-    re_path(r"^holiday/info/$", DateView.as_view())
+    re_path(r"^holiday/year/$", HolidayView.as_view()),  # 获取某年的法定假日
+    re_path(r"^holiday/info/$", DateView.as_view()),  # 获取某天的节假日情况
+    path("holiday/next/"),  # 获取下一个法定假日
 ]
--- apps/apiv2/views.py
+++ apps/apiv2/views.py
@@ -1,5 +1,7 @@
 import datetime
 
+import hutils
+from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -22,9 +24,32 @@ class DateView(APIView):
 
     @staticmethod
     def get(request):
-        date = request.query_params.get("date", datetime.datetime.now().date())
+        date = request.query_params.get("date", datetime.date.today())
         try:
             data, code = Holiday().get_today(str(date))
         except:
             return Response({"status": -1, "data": {"message": "出了点问题，请联系我"}})
         return Response({"status": code, "data": data})
+
+
+def next_holiday(request):
+    """获取下一个法定节假日"""
+    date = datetime.date.today()
+    data, code = Holiday().get_legal_holiday(str(date.year))
+    holiday_list = data["message"]
+    for holiday in holiday_list:
+        duration = holiday["duration"].split("~")
+        start, end = duration[0], duration[1]
+        start_date_str = str(date.year) + "-" + start[:2] + "-" + start[3:5]
+        end_date_str = str(date.year) + "-" + end[:2] + "-" + end[3:5]
+        start_date, end_date = hutils.str_to_date(start_date_str), hutils.str_to_date(end_date_str)
+        if date < start_date:
+            return JsonResponse(
+                {
+                    "status": 0,
+                    "data": {
+                        "duration": holiday["duration"],
+                        "message": f"下一个节假日是{holiday['name']}, 距离今天还有{(start_date - date).days}"
+                    }
+                }
+            )
