c6b9988ebb89fdc22ca96268ebe525e1ad22fba6
ltfred <ltfred@163.com>
2020-10-29 16:22:01 +0800

    style: 改一下代码框的字体

--- izone/__init__.py
+++ izone/__init__.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import, unicode_literals
 from pymysql import install_as_MySQLdb
 
 install_as_MySQLdb()
+
+from .celery import app as celery_app
+__all__ = ("celery_app", )
