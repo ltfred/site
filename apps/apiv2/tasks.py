be98f0e522e9fbfe7f5b828ea2e547118f328037
ltfred <ltfred@163.com>
2020-11-23 14:01:55 +0800

    feat: 更新token时更新update_at

--- apps/apiv2/tasks.py
+++ apps/apiv2/tasks.py
@@ -1,3 +1,5 @@
+import datetime
+
 from celery import shared_task
 
 from apiv2.models import APIToken
@@ -5,4 +7,5 @@ from apiv2.models import APIToken
 
 @shared_task
 def reset_token_visit_count():
-    APIToken.objects.filter(deactivated_at__isnull=True).update(visit_count=0)
+    """每天定时更新token的次数"""
+    APIToken.objects.filter(deactivated_at__isnull=True).update(visit_count=0, update_at=datetime.datetime.now())
